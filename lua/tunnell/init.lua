c118cf4ed381eb56c4456c042ce82e9776e013a8
ghetto <ghetto.ch@gmail.com>
2026-07-11 20:59:18 +0200

    re-insert send <CR> in tunnel_range

--- lua/tunnell/init.lua
+++ lua/tunnell/init.lua
@@ -36,7 +36,7 @@ local function tunnell_range(r)
 	vim.fn.system('tmux paste-buffer -dpr -t ' .. target)
 
 	-- tunnell <CR> to run cell in REPL
-	-- vim.fn.system("tmux send-keys -t " .. target .. " Enter")
+	vim.fn.system('tmux send-keys -t ' .. target .. ' Enter')
 end
 
 -- Tunnells cell to target
