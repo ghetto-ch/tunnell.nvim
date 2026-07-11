a6f12a75ec6b45d718cf3b4efd5f347019fd097c
ghetto <ghetto.ch@gmail.com>
2026-07-12 00:43:49 +0200

    some refactoring

--- lua/tunnell/init.lua
+++ lua/tunnell/init.lua
@@ -5,94 +5,111 @@ local defaults = {
 	cell_header = '# %%',
 }
 
+local default_cell_header
+local get_cell_header
+local get_tmux_target
+local config
+local tunnell_range
+local find_bounds
+local tunnell_paragraph
+local is_function
+local tunnell_function
+local tunnell_cell
+local insert_cell_header
+
 -- Derives a cell header from the buffer's `commentstring` (e.g. '// %s' -> '// %%'),
 -- falling back to `defaults.cell_header` when `commentstring` is empty
-local function default_cell_header()
+default_cell_header = function()
 	if vim.bo.commentstring ~= '' then
 		return vim.bo.commentstring:format('%%')
 	end
 	return defaults.cell_header
 end
 
+-- Buffer-local cell_header/tmux_target, falling back to the language-derived or
+-- global default when unset
+get_cell_header = function()
+	return vim.b.cell_header or default_cell_header()
+end
+
+get_tmux_target = function()
+	return vim.b.tmux_target or defaults.tmux_target
+end
+
 -- Sets buffer-variables `cell_header` and `tmux_target` to values given by user via `vim.fn.input`
-local function config()
+config = function()
 	-- cell_header
 	vim.b.cell_header = vim.fn.input({
 		prompt = 'Cell header: ',
 		-- autocomplete with current cell header if exists, otherwise autocomplete with language default
-		default = vim.b.cell_header and vim.b.cell_header or default_cell_header(),
+		default = get_cell_header(),
 	})
 
 	-- tmux_target
 	vim.b.tmux_target = vim.fn.input({
 		prompt = 'Tmux target pane: ',
 		-- autocomplete with current target if exists, otherwise autocomplete with global
-		default = vim.b.tmux_target and vim.b.tmux_target or defaults.tmux_target,
+		default = get_tmux_target(),
 	})
 end
 
 -- Tunnells range `r` to target
---
 -- Reads `r.line1` and `r.line2`
-local function tunnell_range(r)
-	-- grab range from `r.line1` to `r.line2` and load it into the tmux buffer directly,
-	-- avoiding a `:w !cmd` filter (which always triggers a "press ENTER" prompt)
+tunnell_range = function(r)
 	local lines = vim.api.nvim_buf_get_lines(0, r.line1 - 1, r.line2, false)
 
 	-- let filetypes with REPL-specific quirks (e.g. ghci's ':{'/':}' multiline markers)
-	-- transform the lines before they're sent; see after/ftplugin/haskell.lua
+	-- transform the lines before they're sent; see ftplugin/haskell/init.lua
 	if vim.b.tunnell_wrap then
 		lines = vim.b.tunnell_wrap(lines)
 	end
 
 	vim.fn.system({ 'tmux', 'load-buffer', '-' }, table.concat(lines, '\n'))
 
-	-- tunnell lines
-	local target = vim.b.tmux_target and vim.b.tmux_target or defaults.tmux_target
-	vim.fn.system('tmux paste-buffer -dpr -t ' .. target)
+	-- tunnell lines (argv form, not a shell string: `target` is user-controlled input
+	-- and must not be interpolated into a shell command)
+	local target = get_tmux_target()
+	vim.fn.system({ 'tmux', 'paste-buffer', '-d', '-p', '-r', '-t', target })
 
 	-- tunnell <CR> to run cell in REPL
-	vim.fn.system('tmux send-keys -t ' .. target .. ' Enter')
+	vim.fn.system({ 'tmux', 'send-keys', '-t', target, 'Enter' })
+end
+
+-- Finds the [start_line, end_line] bounds of the block delimited by `pattern`,
+-- excluding the delimiter lines themselves. Falls back to the file's first/last
+-- line when no delimiter is found in that direction. Also returns whether a
+-- delimiter was found above the cursor, for callers that need to special-case it
+-- (e.g. tunnell_cell falling back to tunnell_function)
+find_bounds = function(pattern, back_flags)
+	local found_above = vim.fn.search(pattern, back_flags)
+	local start_line = (found_above == 0) and 1 or found_above + 1
+
+	local found_below = vim.fn.search(pattern, 'nW')
+	local end_line = (found_below == 0) and vim.fn.line('$') or found_below - 1
+
+	return start_line, end_line, found_above ~= 0
 end
 
 -- Tunnells paragraph to target
---
 -- Cursor does not have to be at the start of the paragraph, but anywhere inside it
-local function tunnell_paragraph()
-	-- define start of paragraph
+tunnell_paragraph = function()
 	-- 'b'  search Backward instead of forward
 	-- 'n'  do Not move the cursor
 	-- 'W'  don't Wrap around the end of the file
-	local start_line = vim.fn.search('^$', 'bnW')
-
-	-- if no blank line found above cursor, paragraph starts at top of file. Otherwise,
-	-- paragraph starts one line below the blank line
-	if start_line == 0 then
-		start_line = 1
-	else
-		start_line = start_line + 1
-	end
-
-	-- define end of paragraph
-	local end_line = vim.fn.search('^$', 'nW')
-
-	-- if no blank line found below cursor, cursor is in the last paragraph so end line
-	-- should be the last line of the file. Otherwise, end line is one line above the blank line
-	if end_line == 0 then
-		end_line = vim.fn.line('$')
-	else
-		end_line = end_line - 1
-	end
-
-	-- tunnell paragraph range
+	local start_line, end_line = find_bounds('^$', 'bnW')
 	tunnell_range({ line1 = start_line, line2 = end_line })
 end
 
+-- Helper function. Whether `node`'s type looks like a function/method definition
+is_function = function(node)
+	local t = node:type()
+	return t:match('function') or t:match('method')
+end
+
 -- Tunnells enclosing function to target
---
 -- Cursor must be inside a function; uses treesitter to find the smallest enclosing
 -- node whose type looks like a function/method definition
-local function tunnell_function()
+tunnell_function = function()
 	local node = vim.treesitter.get_node()
 	if not node then
 		print('No treesitter parser/node found at cursor, sending paragraph.')
@@ -100,9 +117,7 @@ local function tunnell_function()
 		return
 	end
 
-	while
-		node and not (node:type():match('function') or node:type():match('method'))
-	do
+	while node and not is_function(node) do
 		node = node:parent()
 	end
 
@@ -125,40 +140,25 @@ local function tunnell_function()
 end
 
 -- Tunnells cell to target
---
 -- Cursor does not have to be on the cell header, but anywhere inside the cell
-local function tunnell_cell()
-	-- load cell_header
-	local cell_header = vim.b.cell_header and vim.b.cell_header
-		or default_cell_header()
+tunnell_cell = function()
+	local cell_header = get_cell_header()
 
 	-- '\V' (very nomagic) makes every character literal except '\', so comment markers
 	-- containing regex-special characters (e.g. '/* %% */', '// %%') are matched as-is
 	local pattern = '\\V' .. vim.fn.escape(cell_header, '\\')
 
-	-- define start of cell
 	-- 'b'  search Backward instead of forward
 	-- 'c'  accept a match at the Cursor position
 	-- 'n'  do Not move the cursor
 	-- 'W'  don't Wrap around the end of the file
-	local start_line = vim.fn.search(pattern, 'bcnW')
+	local start_line, end_line, found = find_bounds(pattern, 'bcnW')
 
 	-- if no header is found above cursor, do nothing
-	if start_line == 0 then
+	if not found then
 		print('No cell header found above cursor, sending function.')
 		tunnell_function()
 		return
-	end
-
-	-- define end of cell
-	local end_line = vim.fn.search(pattern, 'nW')
-
-	-- if no header found below cursor, cursor is in the last cell so end line should be the
-	-- last line of the file. Otherwise, end line is one line above next cell header
-	if end_line == 0 then
-		end_line = vim.fn.line('$')
-	else
-		end_line = end_line - 1
 	end
 
 	-- tunnell cell range
@@ -170,10 +170,9 @@ local function tunnell_cell()
 end
 
 -- Inserts a new line below the cursor containing the cell header, cursor stays in
--- normal mode at the end of it
-local function insert_cell_header()
-	local cell_header = vim.b.cell_header and vim.b.cell_header
-		or default_cell_header()
+-- normal mode at the start of it
+insert_cell_header = function()
+	local cell_header = get_cell_header()
 
 	local row = vim.fn.line('.')
 	vim.api.nvim_buf_set_lines(0, row, row, false, { cell_header })
